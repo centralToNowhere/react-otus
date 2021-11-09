ad2ef2409790e2c1ba3e4a03d484b074d12b360d
RojkovD <rojkov.dmt@gmail.com>
2021-11-09 16:00:33 +0700

    initial commit

---
+++ src/components/error/Error.test.js
@@ -0,0 +1,29 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+/* eslint-disable @typescript-eslint/no-empty-function */
+
+import React from "react";
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import ErrorBoundary from "./Error";
+
+beforeAll(() => {
+  jest.spyOn(console, "error").mockImplementation(() => {});
+  jest.spyOn(console, "log").mockImplementation(() => {});
+});
+
+describe("error boundary tests", () => {
+  it("should render error message", () => {
+    const Child = () => {
+      throw new TypeError("Error boundary works");
+    };
+
+    const { getByText } = render(
+      <ErrorBoundary>
+        <Child />
+      </ErrorBoundary>
+    );
+
+    const errorMessage = getByText("Error boundary works");
+    expect(errorMessage).toBeDefined();
+  });
+});
