e4990444e8818803066c88a5c8f15c6110ed604b
RojkovD <rojkov.dmt@gmail.com>
2021-12-15 14:42:43 +0700

    pureFunctions

--- src/lesson7/pureFunctions.ts
+++ src/lesson7/pureFunctions.ts
@@ -2,18 +2,34 @@
 export type Team = { name: string; score: number };
 
 export const getTopName = (teams: Team[]): string => {
-  //
+  return teams.reduce((prevTeam: Team, currentTeam) => {
+    return prevTeam.score < currentTeam.score ? currentTeam : prevTeam;
+  }).name;
 };
 
 // Задание 2
 export type QsObj = Record<string, string | number | boolean | object>;
 
 export const createQs = (qsObj: QsObj): string => {
-  //
+  return Object.keys(qsObj).reduce((prevKey, currentKey, i) => {
+    return prevKey.concat(`${currentKey}=${qsObj[currentKey]}&`)
+  }, "?").slice(0, -1);
 };
 
 // Задание 3
 
 export const parseQs = (qs: string): QsObj => {
-  //
+  const paramsString = qs.match(/^\?(?<query>.+)$/)?.groups?.query;
+
+  if (!paramsString) {
+    return {};
+  }
+
+  return paramsString.split("&")
+    .reduce((qsObj: QsObj, current: string) => {
+      const keyValue: string[] = current.split("=");
+      qsObj[keyValue[0]] = keyValue[1];
+
+      return qsObj;
+    }, {} as QsObj);
 };
