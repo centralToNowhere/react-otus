164953656c902bd0b92aa514d5aac92614787c7b
RojkovD <rojkov.dmt@gmail.com>
2021-12-22 18:08:12 +0700

    deploy-gh changed

--- deploy-gh.sh
+++ deploy-gh.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 git checkout gh-pages-lesson6 \
-&& git merge lesson6 --no-edit \
+&& git merge lesson8 --no-edit \
 && rm -rf docs/* \
 && npm run check \
 && npm run build \
@@ -9,4 +9,4 @@ git checkout gh-pages-lesson6 \
 && git add . \
 && git commit -m "updated github pages" --no-verify \
 && git push origin gh-pages-lesson6 --no-verify \
-&& git checkout lesson6
+&& git checkout lesson8
