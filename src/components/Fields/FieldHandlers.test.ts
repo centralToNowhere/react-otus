8401d0d856303bcd1dde1680a3fae116d839d6b8
RojkovD <rojkov.dmt@gmail.com>
2022-03-10 03:59:30 +0700

    lint fix

--- src/components/Fields/FieldHandlers.test.ts
+++ src/components/Fields/FieldHandlers.test.ts
@@ -1,4 +1,4 @@
-import { renderHook, act } from "@testing-library/react-hooks";
+import { renderHook } from "@testing-library/react-hooks";
 import { useOnChangeHandler } from "@/components/Fields/FieldHandlers";
 import {
   isValidCellSizeString,
